c3931b1de491779d0372052f7eef2bcf117881ce
Skylixx0 <91562212+Skylixx0@users.noreply.github.com>
2024-05-21 08:03:09 -0400

    Deleted unnecessary files

--- FinalPrep/test.cpp
+++
@@ -1,8 +0,0 @@
-#include "test.h"
-#include <iostream>
-using namespace std;
-namespace alik{
-    std::ostream& Test::display(std::ostream& out){
-        out << "your name is: " << this
-    };
-}
